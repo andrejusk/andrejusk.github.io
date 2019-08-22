6dfecd1afb50214aced5d9cd535f1eb54579273b
Andrejus Kostarevas <hi@andrejus.uk>
2019-08-22 22:13:31 +0100

    [#57] PostCSS'ify even more, start layout refactor

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,22 @@
 module.exports = {
-    plugins: [
-      require('autoprefixer')
-    ]
+    plugins: {
+      'autoprefixer': {
+
+      },
+      'postcss-font-magician': {
+        variants: {
+          'Raleway': {
+            '300': [],
+            '400': [],
+            '700': []
+          },
+          'Merriweather': {
+            '300': [],
+            '400': [],
+            '700': []
+          },
+        },
+        foundries: ['google']
+      }
+    }
   }
