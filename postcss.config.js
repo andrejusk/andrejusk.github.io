0811849fb9142aa8f7911a43172080edc84e1ec7
Andrejus <hi@andrejus.uk>
2019-08-23 00:01:38 +0100

    [#57] webpack-ify
    https://github.com/netlify-templates/victor-hugo/

--- postcss.config.js
+++ postcss.config.js
@@ -1,22 +1,5 @@
 module.exports = {
-    plugins: {
-      'autoprefixer': {
-
-      },
-      'postcss-font-magician': {
-        variants: {
-          'Raleway': {
-            '300': [],
-            '400': [],
-            '700': []
-          },
-          'Merriweather': {
-            '300': [],
-            '400': [],
-            '700': []
-          },
-        },
-        foundries: ['google']
-      }
-    }
+  plugins: {
+    'autoprefixer': { },
   }
+}
